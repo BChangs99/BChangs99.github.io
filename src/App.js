7780e184670e92d2175a2a8fcffd3053b726ab51
BChangs99 <yy3chang@edu.uwaterloo.ca>
2022-08-23 17:24:37 -0700

    - Add footer - Add icon attribution

--- src/App.js
+++ src/App.js
@@ -56,6 +56,16 @@ function App() {
         </NavLink>
       </nav>
       <Outlet />
+      <footer className='footer'>
+        <div className='footer-text'>
+          Resource Credits
+          <ul className='footer-text-links'>
+            <li><a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/download" title="download icons">Download icons created by Debi Alpa Nugraha - Flaticon</a></li>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 }
