fff4ca4fdde0d6ddb0668caf9221e0e884748564
BChangs99 <yy3chang@edu.uwaterloo.ca>
2024-06-09 15:31:10 -0700

    Update GA code

--- src/usePageTracking.js
+++ src/usePageTracking.js
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 
-const usePageTracking = () => { 
-   const location = useLocation();
+const usePageTracking = () => {
+  const location = useLocation();
 
-   useEffect(() => {
-      ReactGA.initialize("UA-238977152-1");
-      ReactGA.pageview(location.pathname + location.search);
-   }, [location]);
-}
+  useEffect(() => {
+    ReactGA.initialize("G-Z428J6SS1Z");
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+};
 
 export default usePageTracking;
