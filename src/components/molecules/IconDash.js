d8a2a9c9f350a4cc623bb115bd7202360017324c
BChangs99 <yy3chang@edu.uwaterloo.ca>
2024-06-09 15:59:40 -0700

    - Formatting and GA - Add resume on click event - format header and footer - format resume - small visual update to projects section

--- src/components/molecules/IconDash.js
+++ src/components/molecules/IconDash.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
-import ArticleIcon from '@mui/icons-material/Article';
-import HomeIcon from '@mui/icons-material/Home';
-import { NavLink } from 'react-router-dom';
-import "./IconDash.css"
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import ArticleIcon from "@mui/icons-material/Article";
+import HomeIcon from "@mui/icons-material/Home";
+import { NavLink } from "react-router-dom";
+import { handleResumeOnClick } from "../../shared";
+import "./IconDash.css";
 
 export default function IconDash(props) {
   return (
-   <div className="app-header-contact">
-      <NavLink to="/"><HomeIcon /></NavLink>
-      <a aria-label="LinkedIn Link" alt="LinkedIn Link" href="https://www.linkedin.com/in/brianchangtron/"><LinkedInIcon/></a>
-      <a aria-label="Github Link" alt="Github Link" href="https://www.github.com/BChangs99"><GitHubIcon/></a>
-      <a aria-label="Email Link" alt="Email Link" href="mailto:bchangs778@gmail.com"><EmailIcon/></a>
-      <NavLink to="/resume"><ArticleIcon /></NavLink>
+    <div className="app-header-contact">
+      <NavLink to="/">
+        <HomeIcon />
+      </NavLink>
+      <a aria-label="LinkedIn Link" alt="LinkedIn Link" href="https://www.linkedin.com/in/brianchangtron/">
+        <LinkedInIcon />
+      </a>
+      <a aria-label="Github Link" alt="Github Link" href="https://www.github.com/BChangs99">
+        <GitHubIcon />
+      </a>
+      <a aria-label="Email Link" alt="Email Link" href="mailto:bchangs778@gmail.com">
+        <EmailIcon />
+      </a>
+      <NavLink to="/resume" onClick={handleResumeOnClick}>
+        <ArticleIcon />
+      </NavLink>
       {props.children}
-   </div>
-  )
+    </div>
+  );
 }
--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 export default function Footer() {
   return (
-   <footer className='footer'>
-      <div className='footer-text'>
-      Resource Credits
-      <ul className='footer-text-links'>
-         <li><a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a></li>
-         <li><a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a></li>
-         <li><a href="https://www.flaticon.com/free-icons/download" title="download icons">Download icons created by Debi Alpa Nugraha - Flaticon</a></li>
-         <li><a href="https://www.flaticon.com/free-icons/internet" title="internet icons">Internet icons created by Freepik - Flaticon</a></li>
-      </ul>
+    <footer className="footer">
+      <div className="footer-text">
+        Resource Credits
+        <ul className="footer-text-links">
+          <li>
+            <a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">
+              Linkedin icons created by riajulislam - Flaticon
+            </a>
+          </li>
+          <li>
+            <a href="https://www.flaticon.com/free-icons/github" title="github icons">
+              Github icons created by Pixel perfect - Flaticon
+            </a>
+          </li>
+          <li>
+            <a href="https://www.flaticon.com/free-icons/download" title="download icons">
+              Download icons created by Debi Alpa Nugraha - Flaticon
+            </a>
+          </li>
+          <li>
+            <a href="https://www.flaticon.com/free-icons/internet" title="internet icons">
+              Internet icons created by Freepik - Flaticon
+            </a>
+          </li>
+        </ul>
       </div>
-   </footer>
-  )
+    </footer>
+  );
 }
